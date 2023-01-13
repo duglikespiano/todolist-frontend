e6b3c147ed9bc48e7959430b423b7011aeeb21ad
SUHO PARK <duglikespiano@gmail.com>
2023-01-13 18:13:16 +0900

    INIT : TODO LIST WITH BACKEND

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import TodoCardBox from './components/TodoCardBox';
+
+function App() {
+	return (
+		<Fragment>
+			<TodoCardBox />
+		</Fragment>
+	);
+}
+
+export default App;
---
+++ src/components/TodoCardBox.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+import TodoCard from './TodoCard.jsx';
+
+export default function TodoCardBox() {
+	return (
+		<Fragment>
+			<TodoCard />
+		</Fragment>
+	);
+}
